8b0f85376956519ae1bfba1cfba7129491c3b9e8
Joingmin Kim <expert0226@gmail.com>
2015-10-28 20:03:33 +0900

    JUnit 테스트에 SessionStatus 넣어서 오류 해결

--- workspace_springjava/paradise/src/test/java/com/heaven/mvc/board/controller/TestBoardController.java
+++ workspace_springjava/paradise/src/test/java/com/heaven/mvc/board/controller/TestBoardController.java
@@ -11,6 +11,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.validation.BeanPropertyBindingResult;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.support.SessionStatus;
 
 import com.heaven.mvc.board.domain.BoardVO;
 import com.heaven.mvc.board.service.BoardService;
@@ -21,7 +22,8 @@ public class TestBoardController {
 	@Test
 	public void postWriteOK() {
 		BoardController controller = new BoardController();
-		BoardService service = mock(BoardService.class);		
+		BoardService service = mock(BoardService.class);
+		SessionStatus sessionStatus = mock(SessionStatus.class);
 		controller.setBoardService(service);
 		
 		BoardVO boardVO = new BoardVO();
@@ -31,7 +33,7 @@ public class TestBoardController {
 		
 		//Model model = new ExtendedModelMap();
 		
-		String viewName = controller.write(boardVO, result);
+		String viewName = controller.write(boardVO, result, sessionStatus);
 		//문제점 BindingResult 에 강제 에러를 설정해 주지 않는 이상 필수 항목 값을 다 입력하지 않아도 통과
 		assertThat(viewName, is("redirect:/board/list"));
 		// service 의 메서드 중에 boardVO 를 인자로하는 write() 메서드가 호출된 적이 있는지 검사
@@ -42,6 +44,7 @@ public class TestBoardController {
 	public void postWriteError() {
 		BoardController controller = new BoardController();
 		BoardService service = mock(BoardService.class);		
+		SessionStatus sessionStatus = mock(SessionStatus.class);	
 		controller.setBoardService(service);
 		
 		BoardVO boardVO = new BoardVO();
@@ -52,7 +55,7 @@ public class TestBoardController {
 		
 		//Model model = new ExtendedModelMap();
 		
-		String viewName = controller.write(boardVO, result);
+		String viewName = controller.write(boardVO, result, sessionStatus);
 		assertThat(viewName, is("/board/write"));
 		//verify(service).write(boardVO);
 	}
